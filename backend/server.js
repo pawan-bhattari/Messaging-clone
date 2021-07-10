ea2d89070c8fd5c0ed1b6d7cd23d50bb69da68f3
Pawan Bhattari <pawanbhattari@Pawans-MacBook-Pro.local>
2021-07-10 23:09:12 +1000

    backend foler

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ if(process.env.NODE_ENV === 'production'){
 
   app.use(express.static('frontend/build'));
 
-  app.get('*' ,(req,res)=>{
+  app.get("*",(req,res)=>{
     
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
      
   //   // "proxy":"http://0.0.0.0:8000",
   })
