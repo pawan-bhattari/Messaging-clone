109c94f7e988e557a57d93a38f0b222f02debaf8
Pawan Bhattari <pawanbhattari@Pawans-MacBook-Pro.local>
2021-07-10 16:40:57 +1000

    first commit

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,13 @@ if(process.env.NODE_ENV === 'production'){
 
   // set static folder 
 
-  app.use(express.static('frontend/build'))
+  app.use(express.static('frontend/build'));
 
   app.get('*' ,(req,res)=>{
     
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
      
-
+  //   // "proxy":"http://0.0.0.0:8000",
   })
 }
 
